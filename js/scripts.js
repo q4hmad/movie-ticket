f139a90dd06e4a2e238066c30c17c24b1e816167
Qudsia Ahmad and Sara Spink <sara@>
2017-07-03 20:34:32 -0700

    call a method on the ticketInfo variable

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,19 @@
 function Ticket(movie, time, age)  {
   this.movie = movie;
   this.time = time;
-  this.age = [];
+  this.age = age;
 }
 
-
+Ticket.prototype.ticketPrice = function() {
+  if (this.movie === "Diehard1") {
+    var price = "$1.00";
+  } else if (this.age >= 65 || this.time === "1100") {
+    var price = "$5.00";
+  } else if (this.age < 65 && this.time !== "1100") {
+    var price = "$8.00";
+  }
+  return (this.movie + ", playing at " + this.time + ", costs " + price);
+}
 
 //Uswer interface logic
 $(document).ready(function() {
@@ -17,7 +26,8 @@ $(document).ready(function() {
   var time = $("input:radio[name=time]:checked").val();
   var age = parseInt($("input.enter-age").val());
   var ticketInfo = new Ticket(movie, time, age);
-  console.log(ticketInfo)
+  console.log(ticketInfo.ticketPrice());
+
 
   });
 });
